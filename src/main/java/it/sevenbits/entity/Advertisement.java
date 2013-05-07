902485d9588f59c56603248f82e6c37209be2435
Julia Ashaeva <julia.ashaeva@gmail.com>
2013-05-07 03:55:40 +0000

    An Advertisement entity has been added
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@10 37479ede-2111-a82d-6642-dbc92489a3ae

---
+++ src/main/java/it/sevenbits/entity/Advertisement.java
@@ -0,0 +1,135 @@
+package it.sevenbits.entity;
+
+/**
+ *Класс, представляющий сущность таблицы User а БД
+ */
+public class Advertisement {
+
+    private Integer id;
+    private Integer categoryId;
+    private Integer userId;
+    private String title;
+    private String text;
+    private String photoFile;
+    private Long createdDate;
+    private Long updatedDate;
+    private Boolean isDeleted;
+
+public Advertisement(){
+    id = 0;
+    categoryId = 0;
+    userId = 0;
+    title = null;
+    text = null;
+    photoFile = null;
+    createdDate = 0l;
+    updatedDate = 0l;
+    isDeleted = false;
+}
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getPhotoFile() {
+        return photoFile;
+    }
+
+    public Long getCreatedDate() {
+        return createdDate;
+    }
+
+    public Long getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public Boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setCategoryId(final Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setUserId(final Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public void setText(final String text) {
+        this.text = text;
+    }
+
+    public void setPhotoFile(final String photoFile) {
+        this.photoFile = photoFile;
+    }
+
+    public void setCreatedDate(final Long createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setUpdatedDate(final Long updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public void setDeleted(final Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Advertisement that = (Advertisement) o;
+
+        if (categoryId != null ? !categoryId.equals(that.categoryId) : that.categoryId != null) return false;
+        if (createdDate != null ? !createdDate.equals(that.createdDate) : that.createdDate != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (isDeleted != null ? !isDeleted.equals(that.isDeleted) : that.isDeleted != null) return false;
+        if (photoFile != null ? !photoFile.equals(that.photoFile) : that.photoFile != null) return false;
+        if (text != null ? !text.equals(that.text) : that.text != null) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (updatedDate != null ? !updatedDate.equals(that.updatedDate) : that.updatedDate != null) return false;
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (categoryId != null ? categoryId.hashCode() : 0);
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + (photoFile != null ? photoFile.hashCode() : 0);
+        result = 31 * result + (createdDate != null ? createdDate.hashCode() : 0);
+        result = 31 * result + (updatedDate != null ? updatedDate.hashCode() : 0);
+        result = 31 * result + (isDeleted != null ? isDeleted.hashCode() : 0);
+        return result;
+    }
+}
