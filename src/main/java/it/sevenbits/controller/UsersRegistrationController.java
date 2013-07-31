7967142d644f415e6261333c594d285a0b1835c5
Dmitry Sibert <dimaaasik.s@gmail.com>
2013-07-31 06:23:56 +0000

    added class which can sending message om emails. Files removing was added.
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@85 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/java/it/sevenbits/controller/UsersRegistrationController.java
+++ src/main/java/it/sevenbits/controller/UsersRegistrationController.java
@@ -1,8 +1,15 @@
 package it.sevenbits.controller;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import it.sevenbits.service.mail.MailSenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +20,22 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "user")
 public class UsersRegistrationController {
 
+    final Logger logger = LoggerFactory
+            .getLogger(AdvertisementController.class);
+
     @RequestMapping(value = "/registration.html", method = RequestMethod.GET)
     public ModelAndView registrationForm() {
 
         ModelAndView modelAndView = new ModelAndView("user/registration");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("application-context-resources.xml");
+        MailSenderService mm = (MailSenderService) context.getBean("mailMail");
+        try{
+        mm.sendMail("naturalexchangeco@gmail.com","naturalexchangeco@gmail.com","test","hello my friend!");
+        } catch(MailException me) {
+             logger.error(me.toString());
+        }
         return modelAndView;
     }
 
