9ca6b44ca60add11dada6d1e8ceddbb9f6b59875
Svetlana Krivosheeva <Ly4chik.Sveta@gmail.com>
2013-08-07 05:29:13 +0000

    added category which worked
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@119 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/webapp/resources/js/saveSearch.js
+++ src/main/webapp/resources/js/saveSearch.js
@@ -38,7 +38,7 @@ function validateEmail(email) {
         for (var i = 0, length = radios.length; i < length; i++) {
             if (radios[i].checked) {
                 // do whatever you want with the checked radio
-                var categorySearch= radios[i];
+                var categorySearch= $(radios[i]).val();
 
                 // only one radio can be logically checked, don't check the rest
                 break;
