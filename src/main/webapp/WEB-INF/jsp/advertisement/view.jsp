906a5c618e28d111cbbe29e414861ed901b2a1b3
Svetlana Krivosheeva <Ly4chik.Sveta@gmail.com>
2013-08-13 05:47:08 +0000

    worked view checkbox
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@170 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/webapp/WEB-INF/jsp/advertisement/view.jsp
+++ src/main/webapp/WEB-INF/jsp/advertisement/view.jsp
@@ -106,7 +106,7 @@
                     </div>
                     <%-- <button class="save"> Сохранить поиск  </button>     --%>
                     <input type="submit" class="search" value="Найти"/>
-                    <div class="cate">
+                    <div class="cate" id="cate">
                         <p class="pcate"> <form:checkbox  id="one1" path="categories" value="games"  /><label for="one1"><span name="cate"></span>Игры</label></p>
                         <p class="pcate"><form:checkbox id="two2" path="categories" value="clothes" /><label for="two2"><span name="cate"></span>Одежда</label></p>
                         <p class="pcate"> <form:checkbox id="three3" path="categories" value="notclothes"  /><label for="three3"><span name="cate"></span>Не одежда</label></p>
--- src/main/webapp/resources/js/checkbox.js
+++ src/main/webapp/resources/js/checkbox.js
@@ -3,15 +3,13 @@ $(document).ready(function() {
     $("#cate").click(function ()  {
         var cates = document.getElementsByName('cate');
         var checks = document.getElementsByName('categories');     //массив боксов
-                    for (var i = 0, length = checks.length; i < length; i++) {
-                       if (checks[i].checked)  {
-                           $(cates[i]).css('backgroundImage' ,  'url(../resources/images/checkbox_selected.png)') ; }
-                       else {
-                           $(cates[i]).css('backgroundImage' ,  'url(../resources/images/checkbox.png)') ;
-                       }
-
-                    }
-
+        for (var i = 0, length = checks.length; i < length; i++) {
+            if (checks[i].checked)  {
+                $(cates[i]).css('backgroundImage' ,  'url(../resources/images/checkbox_selected.png)') ; }
+            else {
+                $(cates[i]).css('backgroundImage' ,  'url(../resources/images/checkbox.png)') ;
+            }
+        }
     }) ;
 
 } );
