d137f31cacd3e870f7218d0cfcd0ab285f3eab56
Dmitry Sibert <dimaaasik.s@gmail.com>
2013-09-09 13:04:05 +0000

    fixed vk authorization by ajax
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@215 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/webapp/resources/js/vkAuth.js
+++ src/main/webapp/resources/js/vkAuth.js
@@ -11,7 +11,7 @@ function vkAuthButtonFunc(eventObject) {
     var client_id = "client_id=3862800&";
     var scope = "scope=notify&";
     var redirect_uri = "redirect_uri=http://naturalexchange.ru/advertisement/list.html&";
-    var display = "display=DISPLAY&";
+    var display = "display=popup&";
     var response_type = "response_type=token";
     var data = client_id + scope + redirect_uri + display + response_type;
     $.ajax({
