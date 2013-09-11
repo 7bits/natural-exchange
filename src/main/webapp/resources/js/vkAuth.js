8a3575522ffbdd2591d01d38064ef30902606ace
Dmitry Sibert <dimaaasik.s@gmail.com>
2013-09-11 09:08:49 +0000

    test auth script
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@239 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/webapp/resources/js/vkAuth.js
+++ src/main/webapp/resources/js/vkAuth.js
@@ -55,7 +55,7 @@ function myAuth() {
                     window.location.replace("http://naturalexchange.ru/advertisement/list.html");
                 } else if (result.success == "false") {
                     var script = document.createElement('SCRIPT');
-                    script.src = "https://api.vk.com/method/getProfiles?uid=" + user_id + "&v=5.0&access_token=" + access_token + "&callback=callbackFunc'";
+                    script.src = "https://api.vk.com/method/getProfiles?uid=" + user_id + "&v=5.0&access_token=" + access_token + "&callback=callbackFunc";
                     document.getElementsByTagName("head")[0].appendChild(script);
                 }
             }
