bca259eacb3740f97bf23b12895badf74e4bb63d
Dmitry Sibert <dimaaasik.s@gmail.com>
2013-09-27 10:11:01 +0000

    added jQuery script
    git-svn-id: https://n-exchange.googlecode.com/svn/trunk@291 37479ede-2111-a82d-6642-dbc92489a3ae

--- src/main/webapp/resources/js/saveSearch.js
+++ src/main/webapp/resources/js/saveSearch.js
@@ -84,7 +84,7 @@ function validateEmail(email) {
     "use strict";
     var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return reg.test(email);
-  }
+}
 
 function validateCaptcha (captchaInput) {
     "use strict";
