fcd24b6b48c2c4d305b0c19bc8ea841035445592
Alexey <nikitinlexey@gmail.com>
2014-07-30 10:40:03 +0700

    Add right migration

--- src/main/resources/release_data/migration280.sql
+++ src/main/resources/release_data/migration280.sql
@@ -6,8 +6,10 @@ UPDATE advertisement SET is_del = is_deleted;
 ALTER TABLE advertisement DROP is_deleted;
 ALTER TABLE advertisement CHANGE is_del is_deleted TINYINT(1);
 
-ALTER TABLE advertisement ADD COLUMN is_vis TINYINT(1) AFTER is_visible;
-UPDATE advertisement SET is_vis = is_visible;
-ALTER TABLE advertisement DROP is_visible;
-ALTER TABLE advertisement CHANGE is_vis is_visible TINYINT(1);
+ALTER TABLE advertisement ADD COLUMN is_visible TINYINT(1) AFTER is_new;
+UPDATE advertisement SET is_visible = is_new;
+ALTER TABLE advertisement DROP is_new;
+
+UPDATE advertisement SET is_visible=1 WHERE is_deleted=0;
+UPDATE advertisement SET is_visible=0 WHERE is_deleted=1;
 
