0270ad1eedc9fbbf4f3ce2cefa1c01ff2871472d
Evgeniy <rocketjump07@gmail.com>
2014-08-04 15:56:25 +0700

    script upgraded

--- restartserver.sh
+++ restartserver.sh
@@ -2,7 +2,7 @@
 
 ssh root@office.7bits.it -p 22522 -A 'cd /home/n-exchange/
 git pull origin master
-mvn package -Dserver=prod
+mvn clean package -Dserver=prod
 cp /home/n-exchange/target/n-exchange-1.0.0.war /var/lib/tomcat7/webapps/ROOT.war
 /etc/init.d/tomcat7 restart
 '
