058182664b60375b1a3caf01eec245583fc0f5f2
Evgeniy <rocketjump07@gmail.com>
2014-08-05 18:20:36 +0700

    almost working version VK entry, need advice about redirect

--- src/main/java/it/sevenbits/controller/User/VKAuthorizationController.java
+++ src/main/java/it/sevenbits/controller/User/VKAuthorizationController.java
@@ -1,22 +1,37 @@
 package it.sevenbits.controller.User;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import it.sevenbits.controller.UsersController;
 import it.sevenbits.dao.UserDao;
 import it.sevenbits.entity.User;
 import it.sevenbits.security.MyUserDetailsService;
 import it.sevenbits.service.mail.MailSenderService;
 import it.sevenbits.util.TimeManager;
+import it.sevenbits.util.form.VkEntryEmailForm;
+import it.sevenbits.util.form.validator.VkEntryEmailFormValidator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.json.simple.JSONObject;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 /**
@@ -31,6 +46,11 @@ import javax.annotation.Resource;
 @RequestMapping(value = "VK")
 public class VKAuthorizationController {
 
+    /**
+     * Logger for formatter work
+     */
+    private Logger logger = Logger.getLogger(VKAuthorizationController.class);
+
     @Resource(name = "mailService")
     private MailSenderService mailSenderService;
 
@@ -40,55 +60,79 @@ public class VKAuthorizationController {
     @Resource(name = "userDao")
     private UserDao userDao;
 
-//    @RequestMapping(value = "/auth.html", method = RequestMethod.GET)
-//    public ModelAndView vkAuthorization(@RequestParam final String code) {
-//        mailSenderService.sendMail("dimaaasik.s@gmail.com", "GET", code);
-//        ModelAndView modelAndView = new ModelAndView("loginVK");
-//
-//        String op = "https://oauth.vk.com/access_token?client_id=3862800&client_secret=8vaXZngg9Frx7MncBLAO&code=" + code + "&redirect_uri=http://naturalexchange.ru/VK/auth.html";
-//        modelAndView.addObject("vkAuth", op);
-//        return modelAndView;
-//    }
-
-    @RequestMapping(value = "/auth.html", method = RequestMethod.POST)
-    public @ResponseBody JSONObject vkAuthorization2(@RequestBody final String json) {
-        String id = json.replaceAll("=","");
-        JSONObject resultJson = new JSONObject();
-        User user = userDao.findEntityByVkId(id);
+    @RequestMapping(value = "/auth.html", method = RequestMethod.GET)
+    public String vkAuthorization(@RequestParam final String code, final Model model, final RedirectAttributes redirectAttributes) {
+        RestTemplate rest = new RestTemplate();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("client_id", "4491913");
+        map.add("client_secret", "Vvsmg0wg4bLTjBguOjcN");
+        map.add("code", code);
+        map.add("redirect_uri", "http://n-exchange.local/n-exchange/VK/auth.html");
+        Map<String, Object> response = rest.postForObject("https://oauth.vk.com/access_token", map, Map.class);
+        Integer id = (Integer)response.get("user_id");
+        String userId = id.toString();
+        User user = userDao.findEntityByVkId("vk.com/" + userId);
         if (user != null) {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
             UserDetails usrDet = myUserDetailsService.loadUserByUsername(user.getEmail());
             token.setDetails(usrDet);
             SecurityContext context = SecurityContextHolder.getContext();
             context.setAuthentication(token);
-            resultJson.put("success","true");
+            return "redirect:/advertisement/list.html";
         } else {
-            resultJson.put("success","false");
+            MultiValueMap<String, String> vkMap = new LinkedMultiValueMap<String, String>();
+            vkMap.add("uids", userId);
+            vkMap.add("fields", "first_name,last_name");
+            Map<String, Object> userInfo = rest.postForObject("https://api.vk.com/method/users.get", vkMap, Map.class);
+            if (userInfo.containsKey("error")) {
+                return "";
+            }
+            VkEntryEmailForm vkEntryEmailForm = new VkEntryEmailForm();
+            ArrayList<Object> data = (ArrayList<Object>)userInfo.get("response");
+            LinkedHashMap<String, Object> vkResponse = (LinkedHashMap<String, Object>)data.get(0);
+            vkEntryEmailForm.setFirst_name((String)vkResponse.get("first_name"));
+            vkEntryEmailForm.setLast_name((String)vkResponse.get("last_name"));
+            vkEntryEmailForm.setVk_link("vk.com/" + userId);
+            model.addAttribute("VkEntryEmailForm", vkEntryEmailForm);
+            return "VK/auth";
         }
-        return resultJson;
     }
 
-    @RequestMapping(value = "/registration.html", method = RequestMethod.POST)
-    public ModelAndView vkRegistration(@RequestParam final String email,
-                               @RequestParam final String first_name,
-                               @RequestParam final String last_name,
-                               @RequestParam final String id) {
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword("dsfklosdaaevvsdfywewehwehsdu");
-        user.setFirstName(first_name);
-        user.setLastName(last_name);
-        user.setVk_link(id);
-        user.setIsDeleted(false);
-        user.setUpdateDate(TimeManager.getTime());
-        user.setCreatedDate(TimeManager.getTime());
-        user.setRole("ROLE_USER");
-        user.setActivationDate(TimeManager.addDate(UsersController.REGISTRATION_PERIOD));
-        Md5PasswordEncoder md5encoder = new Md5PasswordEncoder();
-        String code = md5encoder.encodePassword(user.getPassword(), user.getEmail() );
-        user.setActivationCode(code);
-        this.userDao.create(user);
-        mailSenderService.sendRegisterMail(user.getEmail(), user.getActivationCode());
-        return new ModelAndView("user/regUserLink");
+//    @RequestMapping(value = "/auth.html", method = RequestMethod.GET)
+//    public ModelAndView vkRegistration(final VkEntryEmailForm vkEntryEmailForm) {
+//        ModelAndView modelAndView = new ModelAndView("/auth");
+//        return modelAndView;
+//    }
+
+    @Autowired
+    private VkEntryEmailFormValidator vkEntryEmailFormValidator;
+
+    @RequestMapping(value = "/auth.html", method = RequestMethod.POST)
+    public String vkRegistrationComplete(final VkEntryEmailForm vkEntryEmailForm,
+        final BindingResult bindingResult, final Model model) {
+        vkEntryEmailFormValidator.validate(vkEntryEmailForm, bindingResult);
+        if (!bindingResult.hasErrors()) {
+            User user = new User();
+            user.setEmail(vkEntryEmailForm.getEmail());
+            user.setPassword("dsfklosdaaevvsdfywewehwehsdu");
+            user.setFirstName(vkEntryEmailForm.getFirst_name());
+            user.setLastName(vkEntryEmailForm.getLast_name());
+            user.setVk_link(vkEntryEmailForm.getVk_link());
+            user.setIsDeleted(false);
+            user.setUpdateDate(TimeManager.getTime());
+            user.setCreatedDate(TimeManager.getTime());
+            user.setRole("ROLE_USER");
+            user.setActivationDate(TimeManager.addDate(UsersController.REGISTRATION_PERIOD));
+            Md5PasswordEncoder md5encoder = new Md5PasswordEncoder();
+            String code = md5encoder.encodePassword(user.getPassword(), user.getEmail());
+            user.setActivationCode(code);
+            this.userDao.create(user);
+            mailSenderService.sendRegisterMail(user.getEmail(), user.getActivationCode());
+            return "VK/vkRegistrationText";
+        } else {
+            vkEntryEmailForm.setEmail("");
+            model.addAttribute("VkEntryEmailForm", vkEntryEmailForm);
+            return "VK/auth";
+        }
     }
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -51,9 +51,10 @@
                 <div class = "registrationButtonsContainer">
                     <div class = "vkEntryContainerLogin">
                         <c:url value="https://oauth.vk.com/authorize" var="vkAuth">
-                            <c:param name="client_id" value="3862800"/>
+                            <%--<c:param name="client_id" value="3862800"/>--%>
+                            <c:param name="client_id" value="4491913"/>
                             <c:param name="scope" value="notify"/>
-                            <c:param name="redirect_uri" value="http://naturalexchange.ru/VK/auth.html"/>
+                            <c:param name="redirect_uri" value="http://n-exchange.local/n-exchange/VK/auth.html"/>
                             <c:param name="display" value="popup"/>
                             <c:param name="response_type" value="code"/>
                         </c:url>
