715a84f67cef2616ced4f6516eeadcf813a1be4f
Evgeniy <rocketjump07@gmail.com>
2014-08-14 18:17:54 +0700

    removed fail from script

--- restartserver.sh
+++ restartserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh ssh root@178.62.39.63 -A 'cd /home/n-exchange/
+ssh root@178.62.39.63 -A 'cd /home/n-exchange/
 git pull origin master
 mvn clean package -Dserver=prod
 cp /home/n-exchange/target/n-exchange-1.0.0.war /var/lib/tomcat7/webapps/ROOT.war
