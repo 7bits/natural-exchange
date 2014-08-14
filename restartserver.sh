9c22d28a275a5a5201acda8495de7d7d3d042ab4
Evgeniy <rocketjump07@gmail.com>
2014-08-14 18:17:15 +0700

    from root, not from user, deploy

--- restartserver.sh
+++ restartserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh root@office.7bits.it -p 22522 -A 'cd /home/n-exchange/
+ssh ssh root@178.62.39.63 -A 'cd /home/n-exchange/
 git pull origin master
 mvn clean package -Dserver=prod
 cp /home/n-exchange/target/n-exchange-1.0.0.war /var/lib/tomcat7/webapps/ROOT.war
