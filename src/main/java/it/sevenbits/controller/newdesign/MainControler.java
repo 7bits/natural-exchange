24eb3d442ac993e17d6885c165854db6cc13cbd1
Alexey Nikitin <nikitinlexey@gmail.com>
2014-08-15 13:16:41 +0700

    main page ready

--- src/main/java/it/sevenbits/controller/newdesign/MainControler.java
+++ src/main/java/it/sevenbits/controller/newdesign/MainControler.java
@@ -2,8 +2,17 @@ package it.sevenbits.controller.newdesign;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "new")
 public class MainControler {
+
+
+    @RequestMapping(value = "/main.html", method = RequestMethod.GET)
+    public ModelAndView mainPage() {
+        return new ModelAndView("main.jade");
+    }
+
 }
