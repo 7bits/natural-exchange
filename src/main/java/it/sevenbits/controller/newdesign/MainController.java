a260f0f04002f76a3938549237dcca16374ad30a
Alexey Nikitin <nikitinlexey@gmail.com>
2014-08-18 18:29:35 +0700

    subscribe doesn't work

--- src/main/java/it/sevenbits/controller/newdesign/MainController.java
+++ src/main/java/it/sevenbits/controller/newdesign/MainController.java
@@ -2,12 +2,17 @@ package it.sevenbits.controller.newdesign;
 
 import it.sevenbits.dao.AdvertisementDao;
 import it.sevenbits.dao.CategoryDao;
+import it.sevenbits.dao.SubscriberDao;
 import it.sevenbits.entity.Advertisement;
 import it.sevenbits.entity.Category;
+import it.sevenbits.entity.Subscriber;
 import it.sevenbits.util.SortOrder;
+import it.sevenbits.util.form.MailingNewsForm;
+import it.sevenbits.util.form.validator.MailingNewsValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +32,12 @@ public class MainController {
     @Autowired
     private CategoryDao categoryDao;
 
+    @Autowired
+    private SubscriberDao subscriberDao;
+
+    @Autowired
+    private MailingNewsValidator mailingNewsValidator;
+
     @RequestMapping(value = "/main.html", method = RequestMethod.GET)
     public ModelAndView mainPage() {
         ModelAndView modelAndView = new ModelAndView("main.jade");
@@ -42,6 +53,33 @@ public class MainController {
         pageList.setPageSize(MAIN_ADVERTISEMENTS);
         pageList.setPage(0);
         modelAndView.addObject("advertisements", pageList.getPageList());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/main.html", method = RequestMethod.POST)
+    public ModelAndView subscribe(
+            final MailingNewsForm mailingNewsFormParam,
+            final BindingResult bindingResult) {
+        ModelAndView modelAndView = this.mainPage();
+
+//        modelAndView.addObject("isAnonym", true);
+//        modelAndView.addObject("isNotUser", true);
+//        modelAndView.addObject("isNotSubscriber", true);
+        if (mailingNewsFormParam.getEmailNews() != null) {
+            Subscriber newSubscriber = new Subscriber(mailingNewsFormParam.getEmailNews());
+            mailingNewsValidator.validate(mailingNewsFormParam, bindingResult);
+            if (!bindingResult.hasErrors()) {
+                MailingNewsForm mailingNewsForm = new MailingNewsForm();
+                if (this.subscriberDao.isExists(newSubscriber)) {
+                    mailingNewsForm.setEmailNews("Вы уже подписаны.");
+                } else {
+                    modelAndView.addObject("isNotSubscriber", "true");
+                    this.subscriberDao.create(newSubscriber);
+                    mailingNewsForm.setEmailNews("Ваш e-mail добавлен.");
+                }
+                modelAndView.addObject("mailingNewsForm", mailingNewsForm);
+            }
+        }
         return modelAndView;
     }
 
