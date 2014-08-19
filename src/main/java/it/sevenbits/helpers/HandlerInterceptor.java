b9dd0a4bd01c8473cd913f48bbbd9797377d7a6c
Alexey Nikitin <nikitinlexey@gmail.com>
2014-08-19 19:39:40 +0700

    add handler

--- src/main/java/it/sevenbits/helpers/HandlerInterceptor.java
+++ src/main/java/it/sevenbits/helpers/HandlerInterceptor.java
@@ -1,5 +1,8 @@
 package it.sevenbits.helpers;
 
+import it.sevenbits.helpers.jadeHelpers.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -8,14 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HandlerInterceptor extends HandlerInterceptorAdapter {
 
+    private UrlService urlService = new UrlService();
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
         return super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String relativePath = request.getServletPath();
+        String fullPath = this.urlService.uri(relativePath);
+        String currentPage = urlService.getAfterDomenPath() + relativePath;
+        
+        modelAndView.addObject("getCurrentFullURI", fullPath);
+        modelAndView.addObject("getCurrentRelativeURI", currentPage);
         super.postHandle(request, response, handler, modelAndView);
     }
 }
