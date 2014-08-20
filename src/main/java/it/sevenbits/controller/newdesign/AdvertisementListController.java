d1c4e7e69fca2d70aef06ea7374e7cc3f0f9da21
Evgeniy <rocketjump07@gmail.com>
2014-08-20 21:05:43 +0700

    simply variant dynamic placing advertisement

--- src/main/java/it/sevenbits/controller/newdesign/AdvertisementListController.java
+++ src/main/java/it/sevenbits/controller/newdesign/AdvertisementListController.java
@@ -3,12 +3,23 @@ package it.sevenbits.controller.newdesign;
 
 import it.sevenbits.dao.AdvertisementDao;
 import it.sevenbits.dao.CategoryDao;
+import it.sevenbits.dao.SubscriberDao;
 import it.sevenbits.entity.Advertisement;
 import it.sevenbits.entity.Category;
+import it.sevenbits.entity.Subscriber;
+import it.sevenbits.entity.Tag;
+import it.sevenbits.util.FileManager;
 import it.sevenbits.util.SortOrder;
+import it.sevenbits.util.form.AdvertisementPlacingForm;
+import it.sevenbits.util.form.MailingNewsForm;
+import it.sevenbits.util.form.validator.AdvertisementPlacingValidator;
+import it.sevenbits.util.form.validator.MailingNewsValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +37,9 @@ public class AdvertisementListController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @Autowired
+    private SubscriberDao subscriberDao;
 
     @RequestMapping(value = "/list.html", method = RequestMethod.GET)
     public ModelAndView list(@RequestParam(value = "currentPage", required = false) final Integer previousPage,
@@ -80,6 +94,7 @@ public class AdvertisementListController {
      * Also in touch
      * "next" for next link (true, false)
      * "previous" for previous link (true, false)
+     *
      * @param modelAndView
      * @param currentPage
      * @param pageCount
@@ -156,7 +171,6 @@ public class AdvertisementListController {
     }
 
 
-
     private String[] stringToArray(String str) {
         StringTokenizer token = new StringTokenizer(str);
         String[] allCategories = new String[token.countTokens()];
@@ -202,13 +216,112 @@ public class AdvertisementListController {
         return this.advertisementDao.findAllAdvertisementsWithCategoryAndKeyWordsOrderBy(categories, keyWords, sortOrder, sortColumn);
     }
 
-    @RequestMapping(value = "/placing.html", method = RequestMethod.GET)
-    public ModelAndView enter() {
-        return new ModelAndView("placing.jade");
-    }
-
     @RequestMapping(value = "/view.html", method = RequestMethod.GET)
     public ModelAndView showAdvertisement() {
         return new ModelAndView("view.jade");
+    }
+
+    @Autowired
+    private AdvertisementPlacingValidator advertisementPlacingValidator;
+
+    @RequestMapping(value = "/placing.html", method = RequestMethod.GET)
+    public ModelAndView placingAdvertisement(@RequestParam(value = "id", required = false) final Long id) {
+        ModelAndView modelAndView = new ModelAndView("placing");
+        AdvertisementPlacingForm advertisementPlacingForm = new AdvertisementPlacingForm();
+        advertisementPlacingForm.setCategory("clothes");
+        if (id != null) {
+            Advertisement advertisement = this.advertisementDao.findById(id);
+            advertisementPlacingForm.setCategory(advertisement.getCategory().getName());
+            advertisementPlacingForm.setText(advertisement.getText());
+            advertisementPlacingForm.setTitle(advertisement.getTitle());
+        }
+        modelAndView.addObject("advertisementPlacingForm", advertisementPlacingForm);
+        modelAndView.addObject("isEditing", false);
+        return modelAndView;
+    }
+
+    /**
+     * Placing or editing the advertisement.
+     *
+     * @param advertisementPlacingFormParam parameters from advertisement form
+     * @param result
+     * @param editingAdvertisementId        id of advertisement which will
+     *                                      be editing
+     * @return ModelAndView object
+     */
+    @RequestMapping(value = "/placing.html", method = RequestMethod.POST)
+    public ModelAndView processPlacingAdvertisement(
+            final AdvertisementPlacingForm advertisementPlacingFormParam,
+            final BindingResult result,
+            final Long editingAdvertisementId
+    ) {
+        String defaultPhoto = "no_photo.png";
+        advertisementPlacingValidator.validate(advertisementPlacingFormParam, result);
+        if (result.hasErrors()) {
+            return new ModelAndView("placing");
+        }
+        FileManager fileManager = new FileManager();
+        String photo = null;
+        if (advertisementPlacingFormParam.getImage() == null && editingAdvertisementId == null) {
+            photo = defaultPhoto;
+        } else if (!(advertisementPlacingFormParam.getImage() == null)) {
+            photo = fileManager.savingFile(advertisementPlacingFormParam.getImage());
+        }
+
+        Advertisement advertisement = null;
+        if (editingAdvertisementId == null) {
+            advertisement = new Advertisement();
+            advertisement.setPhotoFile(photo);
+        } else {
+            advertisement = advertisementDao.findById(editingAdvertisementId);
+            if (photo != null) {
+                advertisement.setPhotoFile(photo);
+            }
+        }
+        advertisement.setText(advertisementPlacingFormParam.getText());
+        advertisement.setTitle(advertisementPlacingFormParam.getTitle());
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userName;
+        if (principal instanceof UserDetails) {//Alex: что это черт возьми???
+            userName = ((UserDetails) principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        List<String> tagList = selectTags(advertisementPlacingFormParam.getTags());
+        Set<Tag> newTags = null;
+        if (tagList != null) {
+            newTags = new HashSet<Tag>();
+            for (String newTag : tagList) {
+                if (!newTag.equals("")) {
+                    Tag addingTag = new Tag();
+                    addingTag.setName(newTag);
+                    newTags.add(addingTag);
+                }
+            }
+        }
+        if (editingAdvertisementId != null) {
+            //change tags
+//                Set<TagEntity> existingTags = this.getTagsFromAdvertisementById(editingAdvertisementId);
+
+            this.advertisementDao.update(editingAdvertisementId, advertisement, advertisementPlacingFormParam.getCategory(), newTags);
+        } else {
+            this.advertisementDao.create(advertisement, advertisementPlacingFormParam.getCategory(), userName, newTags);
+        }
+        return new ModelAndView("placingRequest");
+    }
+
+    private List<String> selectTags(String tags) {
+        if (tags == null) {
+            return null;
+        }
+
+        StringTokenizer token = new StringTokenizer(tags);
+        List<String> saparateTags = new ArrayList<String>();
+        int length = token.countTokens();
+        for (int i = 0; i < length; i++) {
+            saparateTags.add(token.nextToken());
+        }
+        return saparateTags;
     }
 }
