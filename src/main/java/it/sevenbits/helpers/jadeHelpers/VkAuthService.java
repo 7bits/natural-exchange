3e4825cff5dc9409fbbf5b663091bdd765821d00
Evgeniy <rocketjump07@gmail.com>
2014-08-21 18:23:48 +0700

    maybe vk entry working

--- src/main/java/it/sevenbits/helpers/jadeHelpers/VkAuthService.java
+++ src/main/java/it/sevenbits/helpers/jadeHelpers/VkAuthService.java
@@ -4,10 +4,9 @@ public class VkAuthService {
     private String redirectUrl = "";
 
     public VkAuthService() {
-        String end = "&";
         UrlService urlService = new UrlService();
-        redirectUrl = "https://oauth.vk.com/authorize" + "?" + "client_id=" + "4491913" + end + "scope=" + "notify" + end
-        + "redirect_uri=" + urlService.uri("/new/VK/auth.html") + end + "display=" + "popup" + end + "response_type=" + "code";
+        redirectUrl = "https://oauth.vk.com/authorize?client_id=4491913&scope=notify&"
+        + "redirect_uri=" + urlService.uri("/new/VK/auth.html") + "&display=popup&response_type=code";
     }
 
     public String getVkUrl() {
